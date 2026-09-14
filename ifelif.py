24b7a4458a673d7dc8704e2b71b6824588e405d1
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-14 17:53:39 +0530

    Add files via upload

---
+++ ifelif.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+x = int (input ("enter a no"))
+y =int (input( "enter a no "))
+if (x>y):
+    print("y is less than x")
+elif (x <y):
+    print (" y is greater than x")
+elif (x==y):
+    print ("they are equal")
