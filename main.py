9b9225599a02de5f70f5aeca48f52f01b1f5c77b
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-14 17:36:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def hello (to= "world"):
+    print ("hello,",to)
+    
+
+hello()
+name= input ("name your")
+hello (name)
+
+def main():
+    name = input (" whats name")
+    hello()
+    
+def hello():
+    print ("hello", name)
+    
+main()
