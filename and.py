897fb86c39fd73b7de9d1a794abc42e89a4e38da
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:06:04 +0530

    Add files via upload

---
+++ and.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+score = int (input (" enter score "))
+
+if score >= 100 and score <= 500 :
+    print (" grade A ")
+else :
+    print ("   grade Fail ")
