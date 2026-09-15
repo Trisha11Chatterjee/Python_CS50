eb91a129e8c572ce5223cc51f42f3ed45e18b598
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 09:03:57 +0530

    Add files via upload

---
+++ url.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+url = input ("URL:").strip()
+username = url.replace ("https://twitter.com/", "")   
+print (f"username :{username}")
