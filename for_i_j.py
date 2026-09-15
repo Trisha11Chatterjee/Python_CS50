43cdd03b2fbf4a604a2877108f27d5b53f63be1c
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:53:06 +0530

    Add files via upload

---
+++ for_i_j.py
@@ -0,0 +1,21 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+    
+def main():
+    square(4)
+def square (size):
+    for i in range (size):
+        for j in range  (size):
+            print ("#", end = "")
+        print()
+        
+main()
+            
+    
