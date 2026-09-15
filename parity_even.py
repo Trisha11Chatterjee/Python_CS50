57effc66774165888ee3a8a370405e18a6dfe84f
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:10:47 +0530

    Add files via upload

---
+++ parity_even.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x = int (input (" enter x"))
+if x % 2 == 0:
+    print ("even")
+else :
+    print ("odd")
