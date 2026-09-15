513660e578f8be48025eb6459a6cea5bdd008bf9
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:24:07 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+    
+for i in [0,1,2,3,4,5]:
+    print ("meow")
+
+
+        
+    
+    
