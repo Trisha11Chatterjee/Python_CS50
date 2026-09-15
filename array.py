6f7ac2666bff312b0f997f4dbca58f39f16fa58d
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:26:29 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,18 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+    
+students = ["hary", "rahul", "ganesh"]
+
+print (students[0])
+print (students[1])
+print (students[2])
+        
+    
+    
