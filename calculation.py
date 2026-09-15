49ce4135db23a401bf349ea6b890eb1a978c1c95
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 09:20:44 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def total(sell,cost,kitkat):
+    return (sell*17 +cost)* 29 + kitkat
+print (total(sell= 100, cost= 50, kitkat= 25), "kitkat")
+    
+
