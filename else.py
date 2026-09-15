8196f0ff0cd3071310d301f9e06bf2f5d8403e8d
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:00:56 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x = int (input (" enter no x "))
+y = int (input (" enter no y"))
+if x >y :
+    print ("x is greater than y")
+else :
+    print ("  x is less than y or equal to y")
