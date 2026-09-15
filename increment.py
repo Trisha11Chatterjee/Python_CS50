56f9269910984e24b4d573c51b0b8f370e9c0274
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:22:12 +0530

    Add files via upload

---
+++ increment.py
@@ -0,0 +1,20 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+
+    
+        
+i=0
+while i<=3:
+    print ("meow")
+    i+=1
+
+
+        
+    
+    
