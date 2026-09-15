06232b893b46a677507999a80b678b8006f6755c
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2026-09-15 08:03:01 +0530

    Add files via upload

---
+++ or.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x = int (input (" enter no x "))
+y = int (input (" enter no y"))
+if x >y or x <y  :
+    print ("x is not equal to y")
+else :
+    print ("  x is equal to y")
